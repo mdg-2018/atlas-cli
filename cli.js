14adb39ed81a0ff0c493ae725fdaedac4d88c66c
matthew.grogan <matthew.grogan@mongodb.com>
2019-08-06 10:55:57 -0400

    initial commit

---
+++ cli.js
@@ -0,0 +1,65 @@
+var parseArgs = require('minimist');
+var AtlasApiClient = require('atlasmanager').AtlasApiClient;
+var fs = require('fs');
+var help = require('./help');
+
+// parse command line arguments and route to appropriate action
+var args = parseArgs(process.argv.slice(2));
+
+
+if (args._.length > 1) {
+    throw new Error("Error: there should only be one argument");
+} else if (args._.length < 1 || args["help"] != null) {
+    help.displayHelpMessage(function (message) {
+        console.log(message);
+        process.exit();
+    });
+} else {
+
+    //setup
+    var atlascluster = new AtlasApiClient(args.projectid, args.apikey, args.username);
+    var clusterdefinition;
+    if(args.clusterdefinition){
+        clusterdefinition = JSON.parse(args.clusterdefinition);
+    } else if(args.clusterdefinitionfile){
+        clusterdefinition = JSON.parse(fs.readFileSync(args.clusterdefinitionfile));
+    }
+
+    switch (args._[0]) {
+        case ("getclusterinfo"):
+            atlascluster.printclusterinfo(args.clustername);
+            break;
+        case ("getclusternames"):
+            atlascluster.getclusternames(function(names){
+                names.forEach((name) => {
+                    console.log(name);
+                })
+            });
+            break;
+        case ("createcluster"):
+            atlascluster.createcluster(clusterdefinition);
+            break;
+        case ("deletecluster"):
+            atlascluster.deletecluster(args.clustername, args.hasOwnProperty("all"));
+            break;
+        case ("modifycluster"):
+            atlascluster.modifycluster(args.clustername, clusterdefinition);
+            break;
+        case ("pausecluster"):
+            atlascluster.pausecluster(args.clustername);
+            break;
+        case ("resumecluster"):
+            atlascluster.resumecluster(args.clustername);
+            break;
+        case ("help"):
+            help.displayHelpMessage(function (message) {
+                console.log(message);
+                process.exit();
+            });
+            break;
+        default:
+            throw new Error("Invalid input");
+            break;
+    }
+
+}
